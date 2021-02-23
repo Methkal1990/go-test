50d6b2ce435eaeddd9ad2e4c8511828c469050a1
Methkal <methkal.khalawi@gmail.com>
2021-02-23 09:22:43 +0000

    test build 3

--- backend/main.go
+++ backend/main.go
@@ -31,5 +31,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
         if name == "" {
                 name = "World"
         }
-        fmt.Fprintf(w, "Hello Methkal %s!\n", name)
+        fmt.Fprintf(w, "Hello Methkal1 %s!\n", name)
 }
